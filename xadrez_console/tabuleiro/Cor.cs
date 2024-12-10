b71d42b8cd4fe33a7da9917ef81a20d766b1ccba
Rian Santos <riansantos355@gmail.com>
2024-12-10 16:24:17 -0300

    começando a implementar o tabuleiro

---
+++ xadrez_console/tabuleiro/Cor.cs
@@ -0,0 +1,13 @@
+namespace xadrez_console.tabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelha,
+        Verde,
+        Laranja,
+    }
+}
---
+++ xadrez_console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,15 @@
+namespace xadrez_console.tabuleiro
+{
+    class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas) {
+            this.colunas = colunas;
+            this.linhas = linhas;
+            pecas = new Peca[linhas, colunas];
+        }
+    }
+}
